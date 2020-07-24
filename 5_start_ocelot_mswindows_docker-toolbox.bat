7513be7eadb0ac61d03767d63fd270f30cafcfd5
act-opacity <63924644+act-opacity@users.noreply.github.com>
2020-07-24 13:46:56 -0400

    Create 5_start_ocelot_mswindows_docker-toolbox.bat
    New startup script for Windows users when Docker Toolbox must be used instead of Docker Desktop.

---
+++ 5_start_ocelot_mswindows_docker-toolbox.bat
@@ -0,0 +1,47 @@
+@echo off
+color 1F & ^
+echo ********************************************************************* & ^
+echo. & ^
+echo.   Starting Ocelot, an Opacity Storage client. & ^
+echo.   If this is your first time running this script & ^
+echo.   this process will take some time, 10-40 minutes, & ^
+echo.   depending on your network speed and computer hardware. & ^
+echo. & ^
+echo.   This script builds and runs the Docker containers required & ^
+echo.   to use Ocelot. & ^
+echo. & ^
+echo.   IMPORTANT! If Docker is not installed, this script will fail. & ^
+echo.   Please cancel (ctrl + c) this script and install Docker first. & ^
+echo.
+echo.   The easiest way to get started is to install Docker Desktop. & ^
+echo.   See here for details: https://docs.docker.com/desktop/ & ^
+echo. & ^
+echo.   To learn more about Docker, visit: https://docs.docker.com/ & ^
+echo. & ^
+echo.   Once the application is ready, it will open your default browser & ^
+echo.   to http://localhost:5000/ which is the link to Ocelot. & ^
+echo. & ^
+echo ********************************************************************* & ^
+echo. & ^
+echo.   Press any key to skip this step. Waiting so you have time to read message above. & ^
+echo. 
+timeout 60
+echo. & ^
+echo.   Running Docker build. & ^
+echo.
+docker-compose build
+echo. & ^
+echo.   Stopping Ocelot services if currently running. & ^
+echo.
+docker-compose stop
+echo. & ^
+echo.   Starting Ocelot. & ^
+echo.
+docker-compose up -d --remove-orphans
+echo. & ^
+echo.   Let's give the web server a few seconds to get going before trying to load Ocelot home page. & ^
+echo.   Press any key to skip this step. & ^
+echo.   If page doesn't load right away once open in browser, simply press F5 to refresh page.
+timeout 10
+start /max http://localhost:5000/
+stop
